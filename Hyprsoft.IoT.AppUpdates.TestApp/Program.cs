3b58ff95107cf37d5e0f373b27491464c2c568de
Hal Blakeslee <hal@hyprsoft.com>
2018-09-19 10:24:44 -0700

    Removed Console.ReadKey() from test app.

--- Hyprsoft.IoT.AppUpdates.TestApp/Program.cs
+++ Hyprsoft.IoT.AppUpdates.TestApp/Program.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Hyprsoft.IoT.AppUpdates.TestApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            using (var cts = new CancellationTokenSource())
+            {
+                Console.CancelKeyPress += (s, e) =>
+                {
+                    cts.Cancel();
+                    e.Cancel = true;
+                };
+                Console.WriteLine($"{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location)}");
+                if (args.Length > 0)
+                    Console.WriteLine($"Command line: {String.Join(", ", args).Trim()}");
 
-            Console.WriteLine($"{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location)}");
-            if (args.Length > 0)
-                Console.WriteLine($"Command line: {String.Join(", ", args).Trim()}");
-            Console.WriteLine();
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+                try
+                {
+                    await Task.Delay(-1, cts.Token);
+                }
+                catch (TaskCanceledException)
+                {
+                }
+                Console.WriteLine("Exiting.");
+            }   // using cancellation token source.
         }
     }
 }
