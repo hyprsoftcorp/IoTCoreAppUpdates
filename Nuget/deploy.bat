4bfaf98906ae748bacc38728ac47b63467456af7
Hal Blakeslee <hal@hyprsoft.com>
2018-12-29 19:41:08 -0800

    Changed toast background colors based on feedback type.

--- Nuget/deploy.bat
+++ Nuget/deploy.bat
@@ -1,3 +1,3 @@
 nuget.exe push -Source "HyprsoftNugetFeed" -ApiKey VSTS ..\Hyprsoft.Iot.AppUpdates\bin\Release\Hyprsoft.IoT.AppUpdates.1.0.5.nupkg
-nuget.exe push -Source "HyprsoftNugetFeed" -ApiKey VSTS ..\Hyprsoft.Iot.AppUpdates.Web\bin\Release\Hyprsoft.IoT.AppUpdates.Web.1.0.16.nupkg
+nuget.exe push -Source "HyprsoftNugetFeed" -ApiKey VSTS ..\Hyprsoft.Iot.AppUpdates.Web\bin\Release\Hyprsoft.IoT.AppUpdates.Web.1.0.17.nupkg
 pause
