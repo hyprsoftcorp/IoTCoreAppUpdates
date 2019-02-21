5f085a893f5034c8f03ae347d2e4008bbf605dcf
Hal Blakeslee <hblakeslee@gmail.com>
2019-02-21 08:42:33 -0800

    Fixed an issue with displaying the manifest uri on the apps list page.

--- Nuget/deploy.bat
+++ Nuget/deploy.bat
@@ -1,3 +1,3 @@
 nuget.exe push -Source "HyprsoftNugetFeed" -ApiKey VSTS ..\Hyprsoft.Iot.AppUpdates\bin\Release\Hyprsoft.IoT.AppUpdates.1.0.11.nupkg
-nuget.exe push -Source "HyprsoftNugetFeed" -ApiKey VSTS ..\Hyprsoft.Iot.AppUpdates.Web\bin\Release\Hyprsoft.IoT.AppUpdates.Web.1.0.23.nupkg
+nuget.exe push -Source "HyprsoftNugetFeed" -ApiKey VSTS ..\Hyprsoft.Iot.AppUpdates.Web\bin\Release\Hyprsoft.IoT.AppUpdates.Web.1.0.24.nupkg
 pause
