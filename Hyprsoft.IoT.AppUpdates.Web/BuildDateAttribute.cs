78446320c6cc58e66898f848f6b54a9e28d331c5
Hal Blakeslee <hal@hyprsoft.com>
2019-03-05 13:12:37 -0800

    Enhanced admin website UI with button groups, icons, and more options to copy guids.  Updated screen shots.

---
+++ Hyprsoft.IoT.AppUpdates.Web/BuildDateAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Hyprsoft.IoT.AppUpdates.Web
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    internal class BuildDateAttribute : Attribute
+    {
+        public BuildDateAttribute(string value)
+        {
+            /* .csproj addition
+             <ItemGroup>
+                <AssemblyAttribute Include="Hyprsoft.Enp.Web.BuildDateAttribute">
+                    <_Parameter1>$([System.DateTime]::UtcNow.ToString("yyyyMMddHHmmss"))</_Parameter1>
+                </AssemblyAttribute>
+            </ItemGroup>
+            */
+            DateTimeUtc = DateTime.ParseExact(value, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None);
+        }
+
+        public DateTime DateTimeUtc { get; }
+    }
+}
---
+++ Hyprsoft.IoT.AppUpdates.Web/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Reflection;
+
+namespace Hyprsoft.IoT.AppUpdates.Web
+{
+    public static class Extensions
+    {
+        #region Methods
+
+        public static string GetAssemblyInformationalVersion(this Type startupType)
+        {
+            return (((AssemblyInformationalVersionAttribute)startupType.GetTypeInfo().Assembly.GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute))).InformationalVersion);
+        }
+
+        public static DateTime GetAssemblyBuildDateTime(this IHtmlHelper helper)
+        {
+            var attribute = Assembly.GetExecutingAssembly().GetCustomAttribute<BuildDateAttribute>();
+            return attribute != null ? attribute.DateTimeUtc : default(DateTime);
+        }
+
+        #endregion
+    }
+}
