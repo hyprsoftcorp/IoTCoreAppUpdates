be64d62a79d0118fe11868ee48ea91594a0da060
Hal Blakeslee <hal@hyprsoft.com>
2019-06-18 08:23:12 -0700

    Updated client-side dependencies.

--- Nuget/deploy.bat
+++ Nuget/deploy.bat
@@ -1,3 +1,3 @@
 rem nuget.exe push -Source "HyprsoftNugetFeed" -ApiKey VSTS ..\Hyprsoft.Iot.AppUpdates\bin\Release\Hyprsoft.IoT.AppUpdates.1.0.15.nupkg
-nuget.exe push -Source "HyprsoftNugetFeed" -ApiKey VSTS ..\Hyprsoft.Iot.AppUpdates.Web\bin\Release\Hyprsoft.IoT.AppUpdates.Web.1.0.36.nupkg
+nuget.exe push -Source "HyprsoftNugetFeed" -ApiKey VSTS ..\Hyprsoft.Iot.AppUpdates.Web\bin\Release\Hyprsoft.IoT.AppUpdates.Web.1.0.37.nupkg
 pause
